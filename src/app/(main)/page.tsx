51cf8c97270264cf3a785a0598c382b6882792e6
hwang min <minhwang7201@naver.com>
2025-08-07 16:30:17 +0900

    🔧 총조회수 아이콘 수정 및 빌드 오류 해결
    - 총조회수 아이콘: 올바른 눈 모양으로 수정
    - 빌드 캐시 정리로 오류 해결
    - 헤더/푸터 로고 색상 통일 완료

--- src/app/(main)/page.tsx
+++ src/app/(main)/page.tsx
@@ -123,7 +123,7 @@ export default async function Home({ searchParams }: PageProps) {
                 <div className="absolute top-4 right-4 text-slate-200 dark:text-slate-800 group-hover:scale-110 transition-transform duration-300">
                   <svg className="w-8 h-8" fill="currentColor" viewBox="0 0 20 20">
                     <path d="M10 12a2 2 0 100-4 2 2 0 000 4z" />
-                    <path fillRule="evenodd" d="M.458 10C1.732 5.943 5.522 3 10 3s8.268 2.943 9.542 7c-1.274 4.057-5.064 7-9.542 7S1.732 14.057.458 10zM5 21h14a2 2 0 002-2V7a2 2 0 00-2-2H5a2 2 0 00-2 2v12a2 2 0 002 2z" clipRule="evenodd" />
+                    <path fillRule="evenodd" d="M.458 10C1.732 5.943 5.522 3 10 3s8.268 2.943 9.542 7c-1.274 4.057-5.064 7-9.542 7S1.732 14.057.458 10zM14 10a4 4 0 11-8 0 4 4 0 018 0z" clipRule="evenodd" />
                   </svg>
                 </div>
               </div>
